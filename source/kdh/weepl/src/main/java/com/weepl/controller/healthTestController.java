2ce74c90bc459fcdaf0a7292bfd2f68c1aa6bce3
kdongheon <981217dong@gmail.com>
2023-06-05 18:02:02 +0900

    {1:1검사 카테고리 추가 및 상황별 검사 카테고리 추가,추가된 카테고리에 선택지 변경} =>완료,  결과창을 db로 보내기 =>진행중

--- source/kdh/weepl/src/main/java/com/weepl/controller/healthTestController.java
+++ source/kdh/weepl/src/main/java/com/weepl/controller/healthTestController.java
@@ -40,13 +40,50 @@ public class HealthTestController {
 	public String doTest1_3() {
 		return "healthTest/applyTest01_3";
 	}
+	@GetMapping(value="applyTest01_4")
+	public String doTest1_4() {
+		return "healthTest/applyTest01_4";
+	}
+	@GetMapping(value="applyTest01_5")
+	public String doTest1_5() {
+		return "healthTest/applyTest01_5";
+	}
+	@GetMapping(value="applyTest01_6")
+	public String doTest1_6() {
+		return "healthTest/applyTest01_6";
+	}
+	@GetMapping(value="applyTest01_7")
+	public String doTest1_7() {
+		return "healthTest/applyTest01_7";
+	}
+	@GetMapping(value="applyTest01_8")
+	public String doTest1_8() {
+		return "healthTest/applyTest01_8";
+	}
+	@GetMapping(value="applyTest01_9")
+	public String doTest1_9() {
+		return "healthTest/applyTest01_9";
+	}
+	@GetMapping(value="applyTest02_1")
+	public String doTest2_1() {
+		return "healthTest/applyTest02_1";
+	}
+	@GetMapping(value="applyTest02_2")
+	public String doTest2_2() {
+		return "healthTest/applyTest02_2";
+	}
+	@GetMapping(value="applyTest02_3")
+	public String doTest2_3() {
+		return "healthTest/applyTest02_3";
+	}
 	
 	@PostMapping(value="testResult")
-	public String sendResult(String testName, Long testSum, Model model) {
+	public String sendResult(String testName, Long testSum,String testStat, Model model) {
 		System.out.println(testName);
 		System.out.println(testSum);
 		model.addAttribute("testName", testName);
 		model.addAttribute("testSum", testSum);
+		model.addAttribute("testStat", testStat);
 		return "healthTest/testResult";
 	}
 	
