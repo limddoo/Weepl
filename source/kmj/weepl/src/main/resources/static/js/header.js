f69bc3e17e21273f05226d5d2033a19d55692d46
MinJongKim <minjong_94@naver.com>
2023-06-15 11:35:58 +0900

    header 완성

--- source/kmj/weepl/src/main/resources/static/js/header.js
+++ source/kmj/weepl/src/main/resources/static/js/header.js
@@ -1,16 +1,15 @@
 
 
 $(document).ready(function(){
-let hiddenContainer = document.getElementById('hiddenContainer');
-	$(".menu").mouseenter(function(){
-		hiddenContainer.style.backgroundColor="white";
-		hiddenContainer.style.zIndex=-1;
+
+	$("#menu").mouseenter(function(){
+		 $("#hiddenContainer").attr("style","display:block; background-color:white; z-index:-1");
+		menu.style.borderBottom = "2px solid rgba(76,76,78,0.3)";
 		$(".depth_1").stop().slideDown(400);
 	}),
-	$(".menu").mouseleave(function(){
-		hiddenContainer.style.display="hidden";
-		hiddenContainer.style.backgroundColor="transparent";
-		hiddenContainer.style.zIndex=-1;
+	$("#menu").mouseleave(function(){
+		 $("#hiddenContainer").attr("style","display:none; backgroundColor:transparent; z-index:-1");
+		menu.style.borderBottom = "";
 		$(".depth_1").stop().slideUp(300);
 	})
 	})
