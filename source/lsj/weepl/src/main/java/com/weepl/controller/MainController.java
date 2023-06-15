8dd46cadadb5bc735470d8b065666e26c6e62467
DESKTOP-VLGP01G\BIG603-03 <qumo109@gmail.com>
2023-06-15 16:43:21 +0900

    디자인 수정중.. 헤더 변경사항 적용

--- source/lsj/weepl/src/main/java/com/weepl/controller/MainController.java
+++ source/lsj/weepl/src/main/java/com/weepl/controller/MainController.java
@@ -8,7 +8,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 	@GetMapping(value = "/")
 	public String main(Model model) {
-		
 		return "main";
+	}
+	
+	@GetMapping(value = "/siteMap")
+	public String siteMap(){
+		return "/etc/siteMap";
+	}
+	
+	@GetMapping(value = "/weeProjectInfo")
+	public String weeProjectInfo(){
+		return "/etc/weeProjectInfo";
+	}
+	
+	@GetMapping(value = "/weeSymbol")
+	public String weeSymbol(){
+		return "/etc/weeSymbol";
 	}
 }
--- source/lsj/weepl/src/main/resources/static/js/header.js
+++ source/lsj/weepl/src/main/resources/static/js/header.js
@@ -1,12 +1,15 @@
 
 
 $(document).ready(function(){
-	$(".menu").mouseenter(function(){
-		$("#hiddenContainer").attr("style","display:block; background-color:white; z-index:-1");
+
+	$("#menu").mouseenter(function(){
+		 $("#hiddenContainer").attr("style","display:block; background-color:white; z-index:-1");
+		menu.style.borderBottom = "2px solid rgba(76,76,78,0.3)";
 		$(".depth_1").stop().slideDown(400);
 	}),
-	$(".menu").mouseleave(function(){
-		$("#hiddenContainer").attr("style","display:none; backgroundColor:transparent; z-index:-1");
-		$(".depth_1").stop().slideUp(300);
-	});
-})
+	$("#menu").mouseleave(function(){
+		 $("#hiddenContainer").attr("style","display:none; backgroundColor:transparent; z-index:-1");
+		menu.style.borderBottom = "";
+		$(".depth_1").stop().slideUp(100);
+	})
+	})
